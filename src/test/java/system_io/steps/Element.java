0c67dc91c036a598c3ee9ff33f892d432929024c
AlexLogino <alexeyjob3@gmail.com>
2026-01-18 19:13:48 +0300

    add logs

--- src/test/java/system_io/steps/Element.java
+++ src/test/java/system_io/steps/Element.java
@@ -2,6 +2,8 @@ package system_io.steps;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import system_io.tests.BaseTest;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -9,6 +11,8 @@ import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.switchTo;
 
 public class Element extends BaseTest {
+
+    private static final Logger log = LoggerFactory.getLogger(Router.class);
 
     private final String locator;
 
@@ -18,24 +22,28 @@ public class Element extends BaseTest {
 
     @Step("element should be visible")
     public Router shouldBeVisible() {
+        log.info("element '{}' should be visible", locator);
         Selenide.$x(locator).shouldBe(visible);
         return Router.getRouter();
     }
 
     @Step("switch to frame")
     public Router switchToFrame() {
+        log.info("switch to frame '{}'", locator);
         switchTo().frame($x(locator).shouldBe(visible));
         return Router.getRouter();
     }
 
     @Step("click on element")
     public Router click() {
+        log.info("click on element '{}'", locator);
         Selenide.$x(locator).click();
         return Router.getRouter();
     }
 
     @Step("element should not be visible")
     public Router shouldNotBeVisible() {
+        log.info("element '{}' should not be visible", locator);
         Selenide.$x(locator).shouldNotBe(visible);
         return Router.getRouter();
     }
--- src/test/java/system_io/steps/Router.java
+++ src/test/java/system_io/steps/Router.java
@@ -1,6 +1,8 @@
 package system_io.steps;
 
 import io.qameta.allure.Step;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import system_io.pages.BasePage;
 import system_io.pages.PopupComponent;
 
@@ -9,6 +11,7 @@ import static com.codeborne.selenide.Selenide.switchTo;
 
 public class Router {
 
+    private static final Logger log = LoggerFactory.getLogger(Router.class);
     private static final Router instance = new Router();
 
     public PopupComponent framePopup = new PopupComponent();
@@ -21,14 +24,16 @@ public class Router {
         return instance;
     }
 
-    @Step("open url")
+    @Step("open URL: {url}")
     public Router openUrl(String url) {
+        log.info("opening URL: {}", url);
         open(url);
         return this;
     }
 
     @Step("switch to default frame")
     public Router switchToDefaultFrame() {
+        log.info("switch to default frame");
         switchTo().defaultContent();
         return this;
     }
